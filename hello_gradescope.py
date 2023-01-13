ed6e2306641b1350b07021d76698ab60945304ed
otto-botto <114207195+otto-botto@users.noreply.github.com>
2023-01-13 11:27:25 -0800

    Add files via upload

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Lora Dushanova
+# OSU Email: dushanol@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 01/18/2023
+# Description: Assignment0 Gradescope file hello
+
+
+def gradescope_test():
+    return 42
